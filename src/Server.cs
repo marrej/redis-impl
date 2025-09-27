de00cbedd347226ac1aca62b5f70c8fd31692636
Marcus Revaj <marcus.revaj@gmail.com>
2025-09-27 16:23:26 +0200

    Bind to Tcp

--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,10 @@
 using System.Net;
 using System.Net.Sockets;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
+Console.WriteLine("Server Starting");
 
-// Uncomment this block to pass the first stage
-// TcpListener server = new TcpListener(IPAddress.Any, 6379);
-// server.Start();
-// server.AcceptSocket(); // wait for client
+TcpListener server = new TcpListener(IPAddress.Any, 6379);
+server.Start();
+Console.WriteLine("Accepting at 6379");
+server.AcceptSocket(); // wait for client
+Console.WriteLine("Finished");
