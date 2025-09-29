19636ca83b545a69cd14b2989ddc736a82b3e433
Marcus Revaj <marcus.revaj@gmail.com>
2025-09-29 10:46:22 +0200

    Add simple storage

--- src/Server.cs
+++ src/Server.cs
@@ -1,3 +1,4 @@
+using System.Data.SqlTypes;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -9,8 +10,9 @@ using System.Threading;
 
 Console.WriteLine("Server Starting");
 
-TcpListener server = new TcpListener(IPAddress.Any, 6379);
+TcpListener server = new(IPAddress.Any, 6379);
 server.Start();
+Storage storage = new();
 Console.WriteLine("Accepting at 6379");
 
 // A communication loop listenting to socket exchanges
@@ -22,21 +24,21 @@ while (true)
     // Resolves all commands sent from the socket in succession
 
     Thread conn = new Thread(ThreadLoop);
-    conn.Start(new CliInput { socket = socket, id = id });
+    conn.Start(new CliInput { Socket = socket, Id = id, Storage = storage });
     id++;
 }
 
 void ThreadLoop(object data)
 {
     CliInput c = (CliInput)data;
-    Console.WriteLine("Connected thread {0}", c.id);
+    Console.WriteLine("Connected thread {0}", c.Id);
     var parser = new Parser { };
-    var interpreter = new Interpreter { };
+    var interpreter = new Interpreter { Storage = c.Storage};
 
     while (true)
     {
         byte[] buffer = new byte[1024];
-        var bytes = c.socket.Receive(buffer);
+        var bytes = c.Socket.Receive(buffer);
         // Disconnect if no more requests are received (the client errors out)
         if (bytes == 0)
         {
@@ -50,13 +52,13 @@ void ThreadLoop(object data)
             Console.WriteLine(i);
             // Responds using SimpleString which is. "+`${ret}`\r\n" at all times
             var response = Encoding.ASCII.GetBytes(i);
-            c.socket.Send(response);
+            c.Socket.Send(response);
             Console.WriteLine("Response Sent");
         }
         catch (Exception e)
         {
-            var r = Encoding.ASCII.GetBytes("+ERROR didn't parse correctly\r\n");
-            c.socket.Send(r);
+            var r = Encoding.ASCII.GetBytes(Types.GetSimpleString("ERROR didn't parse correctly"));
+            c.Socket.Send(r);
             continue;
         }
     }
@@ -64,32 +66,110 @@ void ThreadLoop(object data)
 
 class CliInput
 {
-    required public int id { get; set; }
-    required public Socket socket { get; set; }
+    required public int Id { get; set; }
+    required public Socket Socket { get; set; }
+
+    required public Storage Storage { get; set; }
+}
+
+class Storage
+{
+    readonly Dictionary<string, string> s = [];
+
+    public string Get(string key)
+    {
+        if (!s.TryGetValue(key, out string? value))
+        {
+            throw new Exception("Value not found");
+        }
+        return value;
+    }
+
+    public void Set(string key, string val)
+    {
+        // Currently always overwrites the past input
+        s[key] = val;
+    }
 }
 
 class Interpreter
 {
+    required public Storage Storage;
     public string Interpret(List<string> p)
     {
         var command = p.Count > 0 ? p[0] : "ERROR";
-        var arguments = p.Count > 1 ? p.Slice(1, p.Count-1) : new List<string> { };
-        switch (command)
+        var arguments = p.Count > 1 ? p[1..] : [];
+        return command switch
         {
-            case "PING":
-                return "+PONG\r\n";
-            case "ECHO":
-                return this.Echo(arguments);
-            default:
-                return "+ERROR no command specified\r\n";
-        }
+            // TODO: refactor. Instead of returning direct value, after interpretation, this should just return a Command object that we can execute in some way?
+            "PING" => Types.GetSimpleString("PONG"),
+            "ECHO" => this.Echo(arguments),
+            "GET" => this.Get(arguments),
+            "SET" => this.Set(arguments),
+            _ => Types.GetSimpleString("ERROR no command specified"),
+        };
     }
 
     public string Echo(List<string> arguments)
     {
-        // https://redis.io/docs/latest/develop/reference/protocol-spec/#bulk-strings
+        return Types.GetBulkString(arguments);
+    }
+
+
+    public string Set(List<string> arguments)
+    {
+        if (arguments.Count < 2)
+        {
+            return Types.GetSimpleString("ERROR invalid parameter count");
+        }
+        var key = arguments[0];
+        var val = arguments[1];
+        try
+        {
+            this.Storage.Set(key, val);
+            return Types.GetSimpleString("OK");
+        }
+        catch (Exception)
+        { 
+            return Types.GetSimpleString("ERROR setting val");
+        }
+    }
+
+    public string Get(List<string> arguments)
+    {
+        if (arguments.Count < 1) {
+            return Types.GetSimpleString("ERROR missing key");
+        }
+        try
+        {
+            var val = this.Storage.Get(arguments[0]);
+            return Types.GetBulkString([val]);
+        }
+        catch (Exception)
+        {
+            return Types.GetBulkString(null);
+        }
+    }
+}
+
+class Types
+{
+    // https://redis.io/docs/latest/develop/reference/protocol-spec/#simple-strings
+    public static string GetSimpleString(string i)
+    {
+        return "+" + i + "\r\n";
+    }
+
+    // https://redis.io/docs/latest/develop/reference/protocol-spec/#bulk-strings
+    public static string GetBulkString(List<string>? inputs)
+    {
         var bulkString = "$";
-        foreach (var a in arguments)
+        if (inputs == null)
+        { 
+            return bulkString+(-1).ToString()+"\r\n";
+        }
+
+        foreach (var a in inputs)
         {
             var len = a.Length + "\r\n";
             var content = a + "\r\n";
