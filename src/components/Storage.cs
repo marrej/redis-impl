b2955ce1d5414ab6b20d518358111d50667af836
Marcus Revaj <marcus.revaj@gmail.com>
2025-10-12 18:58:00 +0200

    Use Miliseconds in the UnixTime Id Autogeneration

--- src/components/Storage.cs
+++ src/components/Storage.cs
@@ -396,7 +396,7 @@ namespace RedisImpl
                 }
                 else
                 {
-                    return DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString() + "-0";
+                    return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds().ToString() + "-0";
                 }
             }
 
